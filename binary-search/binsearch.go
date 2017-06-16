7665bf2b30089cf7a8055933e8e85842a0b87789
Andre Zunino <neyzunino@gmail.com>
2017-06-16 10:18:39 -0300

    Incomplete implementation of the binary search and depth-first search algorithms in Go.

---
+++ binary-search/binsearch.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func binary_search(slice []int, elem int) int {
+    si := 0
+    ei := slice.
+    return 1
+}
+
+func main() {
+    values := []int {4, 9, 10, 16, 32, 40, 42, 64, 96, 100}
+    idx := binary_search(values, 16)
+    fmt.Println("idx 16:", idx)
+}
---
+++ tree-search/tsearch.go
@@ -0,0 +1,69 @@
+/**
+ * Tree search using depth-first traversal (based on the depth-first traversal example).
+ *
+ * Go version, based on the C++ implementation done earlier.
+ *
+ * Sample tree:   0
+ *               / \
+ *             1     2
+ *            / \   / \
+ *           3   4 5   6
+ *                / \
+ *               7   8
+ *
+ */
+
+package main
+
+import "fmt"
+
+type Node struct {
+    value int
+    children []Node
+}
+
+const t = []Node{}
+/*
+const tree = []Node{0, {
+    {1, {
+            {3, {}},
+            {4, {}}
+        }
+    },
+    {2, {
+            {5, {
+                    {7, {}},
+                    {8, {}}
+                }
+            },
+            {6, {}}
+        }
+    }
+}}
+*/
+/*
+const Node* tree_search(const Node& node, int value) {
+    std::cout << "searching node " << node.value << "...\n";
+    if (node.value == value) return &node;
+    const Node* found = nullptr;
+    for (const Node& child : node.children) {
+        found = tree_search(child, value);
+        if (found) break;
+    }
+    return found;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Missing search value argument.\n";
+        return EXIT_FAILURE;
+    }
+    int value = std::atoi(argv[1]);
+    auto found = tree_search(tree, value);
+    if (found) {
+        std::cout << "Found it!\n";
+    } else {
+        std::cout << "Did not find it.\n";
+    }
+}
+*/
