e91986a2bd4745eff5ea3d9e60f71b7714497ec9
Andre Zunino <neyzunino@gmail.com>
2021-04-16 09:58:51 -0300

    FizzBuzz implementation in Rust.

---
+++ fizzbuzz/fizzbuzz.rs
@@ -0,0 +1,27 @@
+// FizzBuzz
+//
+// Andre Zunino <neyzunino@gmail.com>
+// 16 April 2021
+
+fn fizzbuzz(n: u32) {
+    for i in 1..n+1 {
+        let by_3 = i % 3 == 0;
+        let by_5 = i % 5 == 0;
+        if !by_3 && !by_5 {
+            println!("{}", i);
+            continue;
+        }
+        if by_3 {
+            print!("Fizz");
+        }
+        if by_5 {
+            print!("Buzz");
+        }
+        println!();
+    }
+}
+
+fn main() {
+    fizzbuzz(50);
+}
+
