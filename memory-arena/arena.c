413a0412fb6399b7e8d6664ec212398bc7b678f5
Andre Zunino <neyzunino@gmail.com>
2025-01-09 13:22:49 -0300

    A naive implementation of a basic arena allocator.

---
+++ memory-arena/arena.c
@@ -0,0 +1,54 @@
+// Naive implementation of an "arena allocator".
+//
+// Andre Zunino <neyzunino@gmail.com>
+// January 2025
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Arena {
+    void *data;
+    size_t capacity;
+    size_t size;
+} Arena;
+
+Arena arena_create(size_t capacity) {
+    return (Arena) {
+        .data = malloc(capacity),
+        .capacity = capacity,
+        .size = 0
+    };
+}
+
+void arena_destroy(Arena *arena) {
+    free(arena->data);
+    arena->data = NULL;
+}
+
+void *arena_alloc(Arena *arena, size_t size) {
+    size_t remaining = arena->capacity - arena->size;
+    if (remaining < size) {
+        printf("can't allocate\n");
+        return NULL;
+    }
+    void *item = arena->data + arena->size;
+    arena->size += size;
+    return item;
+}
+
+int *arena_int(Arena *arena, int value) {
+    int *i = (int *) arena_alloc(arena, sizeof (int));
+    *i = value;
+    return i;
+}
+
+int main() {
+    Arena a = arena_create(8);
+    int *n1 = (int *) arena_alloc(&a, sizeof (int));
+    *n1 = 10;
+    int *n2 = arena_int(&a, 20);
+    /* int *n3 = arena_int(&a, 30); */
+    printf("n1 = %d\n", *n1);
+    printf("n2 = %d\n", *n2);
+    arena_destroy(&a);
+}
